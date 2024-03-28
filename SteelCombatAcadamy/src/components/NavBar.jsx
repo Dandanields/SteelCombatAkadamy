17a5b9c63227fe5d740b2274f2e88a6b66f0f0e3
Dandanields <121231606+Dandanields@users.noreply.github.com>
2024-03-28 22:16:27 +0100

    Test react

---
+++ SteelCombatAcadamy/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import App from "../App";
+
+function Navbar() {
+  const a = "Hello";
+
+  return (
+    <div>
+      <div>{a}</div>
+      <App></App>
+    </div>
+  );
+}
+
+export default Navbar;
